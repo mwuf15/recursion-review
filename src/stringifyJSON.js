fce3f1d95b84f74353507fb70a790cd0d5622fbe
Michael wu <mwuf15@gmail.com>
2020-06-29 12:47:30 -0700

    completed up to array in stringifyJson file

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,45 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //need to stringify a number
+  if ( typeof(obj) === 'number') {
+    return '' + obj;
+  }
+  //need to stringify null
+  if ( obj === null) {
+    return 'null';
+  }
+  //need to stringify booleans
+  if (typeof(obj) === 'boolean') {
+    return '' + obj;
+  }
+  //need to work on stringsngify a string
+  if (typeof(obj) === 'string') {
+    return `"${obj}"`;
+  }
+
+  //need to work on arrays
+  if (Array.isArray(obj)) {
+    //empty arrays
+    var arr = [];
+    if (obj.length === 0) {
+      return '[]';
+    } else {
+      for ( var i = 0; i < obj.length; i++) {
+        arr.push(stringifyJSON(obj[i]));
+      }
+      return `[${arr}]`;
+    }
+  }
+
+
+  //normal arrays
+  //arrays within arrays  [1, 0, -1, -0.3, 0.3, 1343.32, 3345, 0.00011999999999999999],
+  // return `"${arr}"`
+
+  //need to work on objects
+  //empty objects
+  //normal objects
+  // basic nesting
+  // need to skip unstringifiableValues in obj = 'functions': function() {},'undefined': undefined
 };
